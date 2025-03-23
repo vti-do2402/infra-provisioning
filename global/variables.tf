07de1937435c52c4217f59a65169f4a2c5f9982f
Quentin <s3981278@rmit.edu.vn>
2025-03-23 11:51:27 +0700

    update prod folder and add tfvars example

--- global/variables.tf
+++ global/variables.tf
@@ -2,3 +2,15 @@ variable "aws_region" {
   type    = string
   default = "us-west-2"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "tags" {
+  description = "A map of tags to apply to all resources."
+  type        = map(string)
+  default     = {}
+}
